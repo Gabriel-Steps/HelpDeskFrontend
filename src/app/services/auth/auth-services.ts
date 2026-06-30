7250e59f8774e2d97b54e26fc62cedb07dccbd0a
Gabriel-Steps <gabrielpassosfrancisco@gmail.com>
2026-06-30 15:29:11 -0300

    push: add envrionments

--- src/app/services/auth/auth-services.ts
+++ src/app/services/auth/auth-services.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthServices {
-  private API = "SUA-ROTA-API";
+  private API = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
